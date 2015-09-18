3c69bfe4bade4c4ee17dadb00b963675609fcbeb
prabhat thapa <prabhat_thapa@external.mckinsey.com>
2015-09-18 14:21:52 +0530

    Added Single Page refresh + RESTful APIs -User's Delete/ Add/ UserList/ User Info Display

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/userlist', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({}, {}, function(e, docs){
+  	res.json(docs);
+  });
+});
+
+/* Add user */
+router.post('/adduser', function(req, res){
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.insert(req.body, function(err, result) {
+		res.send(
+			(err === null) ? { msg: ''} : { msg: err}
+		);
+	});
+});
+
+/* Delete user */
+router.delete('/deleteuser/:id', function(req, res){
+	var db = req.db;
+	var collection = db.get('userlist');
+	var userToDelete = req.params.id;
+	console.log('------------ delete ----------');
+	collection.remove({'_id': userToDelete}, function(err){
+		res.send((err === null) ? { msg: ''} : {msg: 'error: '+ err});
+	});
 });
 
 module.exports = router;
